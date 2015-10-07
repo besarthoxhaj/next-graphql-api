1ddc1cccddc938de3ab4c12718027985b748ccd0
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-10-07 16:32:18 +0100

    add basic authentication

--- client/playground.js
+++ client/playground.js
@@ -3,10 +3,17 @@ require('isomorphic-fetch');
 import React from 'react';
 import GraphiQL from 'graphiql';
 
+const apiKey = window.location.search
+	.substring(1)
+	.split('&')
+	.find(part => part.startsWith('apiKey'))
+	.split('=')
+	.pop();
+
 const fetcher = (params) => {
 	console.log('GraphiQL submitted', params);
 
-	return fetch('/', {
+	return fetch(`/?apiKey=${apiKey}`, {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(params)
