26a4f1bd44973ea1671a0cbd440cfe5a0406c9e1
Arjun Gadhia <arjungadhia@gmail.com>
2015-10-21 15:18:09 +0100

    Fix weird tabbing

--- test/smoke.js
+++ test/smoke.js
@@ -2,69 +2,69 @@
 
 const query = `
 	fragment Basic on Content {
-	    type: __typename
-	    contentType
-	    id
-	    title
-	    lastPublished
+			type: __typename
+			contentType
+			id
+			title
+			lastPublished
 	}
 
 	fragment Extended on Content {
-	    genre
-	    summary
-	    primaryTag {
-	        id
-	        url
-	        taxonomy
-	        name
-	    }
-	    primaryImage {
-	        src(width: 710)
-	        alt
-	    }
+			genre
+			summary
+			primaryTag {
+					id
+					url
+					taxonomy
+					name
+			}
+			primaryImage {
+					src(width: 710)
+					alt
+			}
 	}
 
 	fragment Related on Content {
-	    relatedContent(limit: 3) {
-	        id
-	        title
-	        genre
-	        primaryTag {
-	            id
-	            url
-	            taxonomy
-	            name
-	        }
-	    }
+			relatedContent(limit: 3) {
+					id
+					title
+					genre
+					primaryTag {
+							id
+							url
+							taxonomy
+							name
+					}
+			}
 	}
 
 	query TopStoriesTest {
-	    popularTopics {
-	        name
-	        url
-	    }
-	    top(region: UK) {
-	        leads: items(limit: 1, type: Article) {
-	            ... Basic
-	            ... Extended
-	            ... Related
-	        }
-	        liveBlogs: items(type: LiveBlog) {
-	            ... Basic
-	            ... Extended
-	            ... on LiveBlog {
-	                status
-	                updates(limit: 1) {
-	                    date
-	                    text
-	                }
-	            }
-	        }
-	        items(from: 1, type: Article) {
-	            ... Basic
-	            ... Extended
-	        }
-	    }
+			popularTopics {
+					name
+					url
+			}
+			top(region: UK) {
+					leads: items(limit: 1, type: Article) {
+							... Basic
+							... Extended
+							... Related
+					}
+					liveBlogs: items(type: LiveBlog) {
+							... Basic
+							... Extended
+							... on LiveBlog {
+									status
+									updates(limit: 1) {
+											date
+											text
+									}
+							}
+					}
+					items(from: 1, type: Article) {
+							... Basic
+							... Extended
+					}
+			}
 	}
 	`;
 
@@ -73,7 +73,7 @@ module.exports = [
 	{
 		headers: {
 			'Accept': 'application/json',
-    	'Content-Type': 'application/json'
+			'Content-Type': 'application/json'
 		},
 		method: 'POST',
 		body: JSON.stringify({
