1b53ec4499e21a5b06058ff878a3bec42596d5d9
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-11-30 15:03:47 +0000

    adding popularArticles query

--- server/lib/backend-adapters/popular-api.js
+++ server/lib/backend-adapters/popular-api.js
@@ -1,14 +1,26 @@
 class PopularAPI {
 	constructor(cache) {
 		this.cache = cache;
+		this.baseUrl = 'https://ft-next-popular-api.herokuapp.com';
+		this.apiKey = process.env.POPULAR_API_KEY;
 	}
 
 	topics(ttl = 50) {
-		const url = 'https://ft-next-popular-api.herokuapp.com/topics?apiKey=' + process.env.POPULAR_API_KEY;
+		const url = `${this.baseUrl}/topics?apiKey=${this.apiKey}`;
 
 		return this.cache.cached('popular-api.topics', ttl, () => {
 			return fetch(url)
-			.then((response) => response.json());
+				.then(response => response.json());
+		});
+	}
+
+	articles(ttl = 50) {
+		const url = `${this.baseUrl}/articles?apiKey=${this.apiKey}`;
+
+		return this.cache.cached('popular-api.articles', ttl, () => {
+			return fetch(url)
+				.then(response => response.json())
+				.then(json => (json.articles || []).map(article => article.uuid));
 		});
 	}
 }
