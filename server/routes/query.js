9949da26ef71ab05939b9103ee053659e0256f0f
Arjun Gadhia <arjungadhia@gmail.com>
2015-12-11 17:08:13 +0000

    Revert "JSON support"
    This reverts commit ed744de90c811edcd0b6fca7f882d752d88d1529.

---
+++ server/routes/query.js
@@ -0,0 +1,33 @@
+import graphql from '../lib/graphql';
+import { logger } from 'ft-next-express';
+
+export default (req, res, next) => {
+
+
+	const flags = res.locals.flags;
+	const query = req.body.query || req.query.query || req.body;
+	const vars = JSON.parse(req.body.variables || '{}');
+
+	if (!Object.keys(query).length) {
+		logger.warn('Empty query supplied');
+		return res.status(400).send();
+	}
+
+	graphql(
+		{
+			mock: flags.mockFrontPage
+		},
+		flags
+	)
+		.fetch(query, vars)
+		.then(data => {
+			if(req.method === 'GET') {
+				res.set({'Surrogate-Control': 'max-age=120,stale-while-revalidate=6,stale-if-error=259200'});
+			}
+			res.json(data);
+		})
+		.catch(errs => {
+			const err = Array.isArray(errs) ? errs.shift() : errs;
+			next(err);
+		});
+};
