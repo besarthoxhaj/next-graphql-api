c35ac3181b4be66d9bfa48cd541340892f61daab
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-01-13 16:48:33 +0000

    don't cache the limited response

--- server/lib/backend-adapters/popular-api.js
+++ server/lib/backend-adapters/popular-api.js
@@ -12,12 +12,10 @@ class PopularAPI {
 		const url = `${this.baseUrl}/topics?apiKey=${this.apiKey}`;
 
 		return this.cache.cached('popular-api.topics', ttl, () => {
-
 			return fetch(url)
 				.then(response => response.json())
-				.then(topics => sliceList(topics, {from, limit}));
-
-		});
+		})
+			.then(topics => sliceList(topics, {from, limit}));
 	}
 
 	articles({from, limit}, ttl = 50) {
@@ -27,8 +25,8 @@ class PopularAPI {
 			return fetch(url)
 				.then(response => response.json())
 				.then(json => (json.articles || []).map(article => article.uuid))
-				.then(articles => sliceList(articles, {from, limit}));
-		});
+		})
+			.then(articles => sliceList(articles, {from, limit}));
 	}
 }
 
