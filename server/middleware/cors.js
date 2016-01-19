c8b63d85c2e1949de6794bd8834628dc6f91f980
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-01-19 16:37:59 +0000

    vary on origin if cors

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -7,6 +7,7 @@ export default (req, res, next) => {
 		res.header('Access-Control-Allow-Credentials', true);
 		res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
 		res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+		res.vary('Origin');
 	}
 	next();
 };
