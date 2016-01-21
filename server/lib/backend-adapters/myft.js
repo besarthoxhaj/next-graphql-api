953901e1d61270734d7f5e95bc9310b533792a90
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-01-21 17:04:06 +0000

    indentation

--- server/lib/backend-adapters/myft.js
+++ server/lib/backend-adapters/myft.js
@@ -2,15 +2,15 @@ import myftClient from 'next-myft-client';
 import sliceList from '../helpers/slice-list';
 
 class Myft {
-    constructor() {
-        this.type = 'myft';
-    }
+	constructor() {
+		this.type = 'myft';
+	}
 
-    getAllRelationship(uuid, relationship, model, args) {
+	getAllRelationship(uuid, relationship, model, args) {
 		return myftClient.getAllRelationship('user', uuid, relationship, model, args)
-            .then(res => { console.log(res.items); return res.items; })
+			.then(res => { console.log(res.items); return res.items; })
 			.catch(err => { console.log(err); return []; });
-    }
+	}
 
 	personalisedFeed(args) {
 		return fetch(`https://ft-next-personalised-feed-api.herokuapp.com/v2/feed/${args.uuid}?originatingSignals=followed&from=-7d`, {
