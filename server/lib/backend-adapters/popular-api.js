ef7ff6adeb047b746ba822eee327661ec048faf0
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-03-01 16:05:36 +0000

    updating popular-api test

--- server/lib/backend-adapters/popular-api.js
+++ server/lib/backend-adapters/popular-api.js
@@ -1,33 +1,29 @@
 import sliceList from '../helpers/slice-list';
 
-class PopularAPI {
+export default class {
 	constructor(cache) {
 		this.cache = cache;
 		this.baseUrl = 'https://ft-next-popular-api.herokuapp.com';
 		this.apiKey = process.env.POPULAR_API_KEY;
 	}
 
-	topics({from, limit}, ttl = 50) {
-
+	topics({ from, limit } = {}, ttl = 50) {
 		const url = `${this.baseUrl}/topics?apiKey=${this.apiKey}`;
 
-		return this.cache.cached('popular-api.topics', ttl, () => {
-			return fetch(url)
-				.then(response => response.json())
-		})
-			.then(topics => sliceList(topics, {from, limit}));
+		return this.cache.cached('popular-api.topics', ttl, () =>
+				fetch(url).then(response => response.json())
+			)
+			.then(topics => sliceList(topics, { from, limit }));
 	}
 
-	articles({from, limit}, ttl = 50) {
+	articles({ from, limit } = {}, ttl = 50) {
 		const url = `${this.baseUrl}/articles?apiKey=${this.apiKey}`;
 
-		return this.cache.cached('popular-api.articles', ttl, () => {
-			return fetch(url)
-				.then(response => response.json())
-				.then(json => (json.articles || []).map(article => article.uuid))
-		})
-			.then(articles => sliceList(articles, {from, limit}));
+		return this.cache.cached('popular-api.articles', ttl, () =>
+				fetch(url)
+					.then(response => response.json())
+					.then(json => (json.articles || []).map(article => article.uuid))
+			)
+			.then(articles => sliceList(articles, { from, limit }));
 	}
 }
-
-export default PopularAPI;
---
+++ test/server/lib/backend-adapters/popular-api.test.js
@@ -0,0 +1,93 @@
+import fetchMock from 'fetch-mock';
+import sinon from 'sinon';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.should();
+chai.use(chaiAsPromised);
+
+import Popular from '../../../../server/lib/backend-adapters/popular-api';
+import Cache from '../../../../server/lib/cache';
+
+const cachedSpy = () => sinon.spy((cacheKey, cacheTTL, value) => value());
+
+describe.only('Popular API', () => {
+
+	describe('Topics', () => {
+
+		before(() => {
+			fetchMock.mock(
+				'^https://ft-next-popular-api.herokuapp.com/topics',
+				[{ id: 'topic-one' }, { id: 'topic-two' }, { id: 'topic-three' }]
+			);
+		});
+
+		after(() => {
+			fetchMock.restore();
+		});
+
+		it('should be able to fetch topics', () => {
+			const cache = { cached: cachedSpy() };
+			const popular = new Popular(cache);
+
+			return popular.topics()
+				.then(topics => {
+					topics.should.have.length(3);
+				})
+		});
+
+		it('should be able limit the number of topics', () => {
+			const cache = { cached: cachedSpy() };
+			const popular = new Popular(cache);
+
+			return popular.topics({ limit: 2 })
+				.then(topics => {
+					topics.should.have.length(2);
+					topics.should.deep.equal([{ id: 'topic-one' }, { id: 'topic-two' }]);
+				})
+		});
+
+		it('should be able offset the topics', () => {
+			const cache = { cached: cachedSpy() };
+			const popular = new Popular(cache);
+
+			return popular.topics({ from: 1 })
+				.then(topics => {
+					topics.should.have.length(2);
+					topics.should.deep.equal([{ id: 'topic-two' }, { id: 'topic-three' }]);
+				})
+		});
+
+		it('should be able offset and limit the topics', () => {
+			const cache = { cached: cachedSpy() };
+			const popular = new Popular(cache);
+
+			return popular.topics({ limit: 1, from: 1 })
+				.then(topics => {
+					topics.should.have.length(1);
+					topics.should.deep.equal([{ id: 'topic-two' }]);
+				})
+		});
+
+		it('should use correct cache key', () => {
+			const cache = { cached: cachedSpy() };
+			const popular = new Popular(cache);
+
+			return popular.topics()
+				.then(() => {
+					cache.cached.alwaysCalledWith('popular-api.topics', 50).should.be.true;
+				})
+		});
+
+		it('should be able to change cache ttl', () => {
+			const cache = { cached: cachedSpy() };
+			const popular = new Popular(cache);
+
+			return popular.topics({}, 100)
+				.then(() => {
+					cache.cached.firstCall.args[1].should.be.equal(100);
+				})
+		});
+
+	});
+
+});
