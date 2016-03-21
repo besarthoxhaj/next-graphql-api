c3063c2550e47889e5423dd5feecb1f7946d584d
Laurie Boyes <laurie.boyes@ft.com>
2016-03-21 11:59:22 +0000

    added tests for capi adapter search

--- test/server/lib/backend-adapters/capi.test.js
+++ test/server/lib/backend-adapters/capi.test.js
@@ -106,4 +106,85 @@ describe('CAPI', () => {
 
 	});
 
+	describe('#search', () => {
+
+		before(() => {
+			fetchMock.mock(
+				new RegExp('https://[^/]*/v3_api_v2/item/_search'),
+				{
+					hits: {
+						total: 1,
+						hits: [
+							{
+								_source: {
+									id: 'topic1'
+								}
+							}
+						]
+					}
+				}
+			);
+		});
+
+		after(() => {
+			fetchMock.restore();
+		});
+
+		it('should be able to fetch content', () => {
+			const cache = {cached: cachedSpy()};
+			const capi = new CAPI(cache);
+
+			return capi.search('metadata.idV1', 'topicId', {count: 50, since: '2016-03-21'})
+				.then(content => {
+					const expectedResult = [{id: 'topic1'}];
+					expectedResult.total = 1;
+					content.should.eql(expectedResult);
+				});
+		});
+
+		it('should use correct cache key and ttl when a single conceptID is specified', () => {
+			const cached = cachedSpy();
+			const cache = {cached};
+			const capi = new CAPI(cache);
+
+			return capi.search('metadata.idV1', 'topicId', {count: 50, since: '2016-03-21'})
+				.then(() => {
+					cached.alwaysCalledWith('capi.search.metadata.idV1:topicId:since_2016-03-21:count_50', 600).should.be.true;
+				});
+		});
+
+		it('should use correct cache key and ttl when multiple conceptIDs are specified', () => {
+			const cached = cachedSpy();
+			const cache = {cached};
+			const capi = new CAPI(cache);
+
+			return capi.search('metadata.idV1', ['topicId1', 'topicId2', 'topicId3'], {count: 50, since: '2016-03-21'})
+				.then(() => {
+					cached.alwaysCalledWith('capi.search.metadata.idV1:topicId1,topicId2,topicId3:since_2016-03-21:count_50', 600).should.be.true;
+				});
+		});
+
+		it('should handle empty response from CAPI', () => {
+			fetchMock.reMock(
+				new RegExp('https://[^/]*/v3_api_v2/item/_search'),
+				{
+					hits: {
+						total: 0,
+						hits: []
+					}
+				}
+			);
+			const cache = {cached: cachedSpy()};
+			const capi = new CAPI(cache);
+
+			return capi.search('metadata.idV1', 'topicId', {count: 50, since: '2016-03-21'})
+				.then(content => {
+					const expectedResult = [];
+					expectedResult.total = 0;
+					content.should.eql(expectedResult);
+				});
+		});
+
+	});
+
 });
