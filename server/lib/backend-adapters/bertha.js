63c231815cf460d78ae4f6fde84b98f64d468c2f
phamann <patrickhamann@gmail.com>
2016-03-22 16:27:53 +0000

    Add new 'Collection' type and 'collections()' endpoint

---
+++ server/lib/backend-adapters/bertha.js
@@ -0,0 +1,19 @@
+export default class {
+	constructor (cache) {
+		this.type = 'bertha';
+		this.cache = cache;
+	}
+
+	get(sheetKey, sheetName, ttl = 60 * 10) {
+		const cacheKey = `${this.type}.sheet.${sheetKey}.${sheetName}`;
+		console.log(`FETCHING CACHED BERTHA: ${cacheKey}`);
+		return this.cache.cached(cacheKey, ttl, () => {
+			console.log('BERTHA CACHE MISS');
+			return fetch(`https://bertha.ig.ft.com/view/publish/gss/${sheetKey}/${sheetName}`, {
+				method: 'get',
+				headers: { 'Content-Type': 'application/json' }
+			})
+			.then(r => r.json())
+		});
+	}
+}
--- server/lib/types/collections.js
+++ server/lib/types/collections.js
@@ -1,8 +1,10 @@
 import { GraphQLString, GraphQLInt, GraphQLList, GraphQLObjectType } from 'graphql';
 
-import { Content } from './content';
+import { Content, Concept } from './content';
 import { ContentType } from './basic';
 import backendReal from '../backend-adapters/index';
+import moment from 'moment';
+import identity from '../identity';
 
 const contentToUiid = content => content.id.replace(/http:\/\/api\.ft\.com\/things?\//, '');
 
@@ -74,4 +76,39 @@ const List = new GraphQLObjectType({
 	}
 });
 
-export { Page, List };
+const Collection = new GraphQLObjectType({
+	name: 'Collection',
+	description: 'Abstract collection of concepts',
+	fields: {
+		title: {
+			type: GraphQLString
+		},
+		concepts: {
+			type: new GraphQLList(Concept),
+			description: 'Concepts'
+		},
+		articleCount: {
+			type: GraphQLInt,
+			description: `
+				Approximate number of articles published with this concept since the given date, up to a
+				maximum value of count (default date is 1 week, default count is 100)`,
+			args: {
+				since: {
+					type: GraphQLString,
+					defaultValue: moment().subtract(7, 'days').format('YYYY-MM-DD')
+				},
+				count: {
+					type: GraphQLInt,
+					defaultValue: 100
+				}
+			},
+			resolve: (collection, { since, count }, { rootValue: { flags, backend = backendReal }}) => {
+				return Promise.all(collection.concepts.map(c => c.id).map(id =>
+					backend(flags).capi.searchCount('metadata.idV1', id, { count, since})
+				)).then(counts => counts.filter(identity).reduce((a, b) => a + b, 0));
+			}
+		}
+	}
+});
+
+export { Page, List, Collection };
