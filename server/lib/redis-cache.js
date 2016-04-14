a83d6aa3680cce0f18a97664869816606c298949
phamann <patrickhamann@gmail.com>
2016-04-14 09:49:20 +0100

    Whitespace

--- server/lib/redis-cache.js
+++ server/lib/redis-cache.js
@@ -3,38 +3,38 @@ import logger from '@financial-times/n-logger';
 import * as redis from './redis';
 
 export default class {
-	constructor (staleTtl, unusedStaleTtl) {
-		this.cache = redis;
-		this.requestMap = [];
+    constructor () {
+        this.cache = redis;
+        this.requestMap = [];
     }
 
-	cached (key, ttl, fetcher) {
-		const metricsKey = key.split('.')[0];
-		const cache = this.contentCache;
+    cached (key, ttl, fetcher) {
+        const metricsKey = key.split('.')[0];
+        const cache = this.contentCache;
 
-		return this.cache.get(key)
-				.then((data) => {
-					// we have fresh data
-					if(data) {
-						metrics.count(`cache.${metricsKey}.cached`, 1);
-						return JSON.parse(data);
-					}
-					// we don't have fresh data, fetch it
-					return this.fetchAndSet(key, ttl, fetcher);
-				});
-	}
+        return this.cache.get(key)
+                .then((data) => {
+                    // we have fresh data
+                    if(data) {
+                        metrics.count(`cache.${metricsKey}.cached`, 1);
+                        return JSON.parse(data);
+                    }
+                    // we don't have fresh data, fetch it
+                    return this.fetchAndSet(key, ttl, fetcher);
+                });
+    }
 
-	fetchAndSet (key, ttl, fetcher) {
-		if(this.requestMap[key]) return this.requestMap[key];
+    fetchAndSet (key, ttl, fetcher) {
+        if(this.requestMap[key]) return this.requestMap[key];
 
-		const metricsKey = key.split('.')[0];
+        const metricsKey = key.split('.')[0];
 
-		metrics.count(`cache.${metricsKey}.fresh`, 1);
+        metrics.count(`cache.${metricsKey}.fresh`, 1);
 
-		this.requestMap[key] = fetcher().then((res) => {
-									if(!res) { return; }
+        this.requestMap[key] = fetcher().then((res) => {
+                                    if(!res) { return; }
 
-									const data = JSON.stringify(res);
+                                    const data = JSON.stringify(res);
 
                                     return this.cache
                                             .setex(key, ttl, data)
@@ -42,14 +42,14 @@ export default class {
                                                 delete this.requestMap[key];
                                                 return res;
                                             });
-								})
-								.catch((err) => {
-									metrics.count(`cache.${metricsKey}.error`, 1);
-									delete this.requestMap[key];
-									logger.error(err);
-								});
+                                })
+                                .catch((err) => {
+                                    metrics.count(`cache.${metricsKey}.error`, 1);
+                                    delete this.requestMap[key];
+                                    logger.error(err);
+                                });
 
-		return this.requestMap[key];
-	}
+        return this.requestMap[key];
+    }
 }
 
